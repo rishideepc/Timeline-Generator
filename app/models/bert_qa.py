d10c6238e3c88ae4fbd46a36da0223a7bdaca90d
rishideepc <rishi1a2@gmail.com>
2023-05-05 23:41:43 +0530

    2023-05-05-Rishideep: bert questions updation

--- app/models/bert_qa.py
+++ app/models/bert_qa.py
@@ -8,8 +8,8 @@ class BertQA:
         # getting pre-trained model
         self.model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
         self.tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-        self.questions = ["What event occurred?", "When did the landslide occur?", "Where did the landslide happen?",
-                          "How many people died?", "How many people were injured?", "How many people were affected?",
+        self.questions = ["What event occurred?", "What was the exact date of the landslide?", "What was the exact location of the landslide?",
+                          "How many people died in the landslide?", "How many people were injured in the landslide?", "How many people overall were affected by the landslide?",
                           "How severe was the landslide?"]
         self.answers = {i:"" for i in range(1,8)}
         self.text = ""
@@ -76,4 +76,50 @@ if __name__ == '__main__':
         "rainfall at Barra do Sahy. A small TV covered in mud was seen in one of the landslides sites. Many buildings " \
         "were damaged due to landslides and flooding. Several deadly landslides were seen after severe rainfall at " \
         "Barra do Sahy, in Sao Sebastiao, Brazil on Tuesday. "
+    
+    test_t ="""
+        More than 120 people were killed in devastating floods and landslides caused by heavy rains in Rwanda, the government said on Wednesday, the highest death toll from a flood reported in a single day in the country’s recent history.
+
+        Entire families were killed, injured or left homeless and in desperate need of assistance.
+
+        “I wanted to cry but couldn’t in front of my children,” said Martine Nsanimana, 40, a resident of a small village in Western Rwanda whose home and farmland were destroyed by the floods.
+
+        “If you saw how the farmland was washed away, you would want to cry,” said Mr. Nsanimana, a father of three.
+
+        The rains started on Tuesday, but residents said that some people were still trapped in their homes on Wednesday, suggesting that the number of deaths could rise. Local officials also warned that more homes could fall down.
+
+        Most of the casualties were recorded in the west and north of Rwanda, but some damage was also reported in the south. The districts of Ngororero, Rubavu, Nyabihu, Rutsiro, and Karongi, in the northwest, were among the hardest hit, the Rwandan government said.
+
+        Videos showed swollen rivers of mud streaming through villages and alongside houses, and landslides of mud and rocks racing down hillsides into roads, homes and infrastructure.
+
+        “Many houses collapsed on people,” said Francois Habitegeko, the governor of the Rwanda’s Western Province.
+
+        Emergency workers were deployed to rescue those caught by the floods, helping the injured and those trapped in their homes.
+
+        April is typically Rwanda’s rainiest month, but even for April the rains last month were heavy. And while the rainy season usually begins to wind down in May, the forecast called for more rain in the coming days.
+
+        Experts said that the sandy soil and the terrain in the areas that were hit made them susceptible to floods and landslides.
+
+        Joseph Tuyishimire, a researcher in geography at the University of Rwanda, said that the Western and Northern Provinces used to be natural forests, but had been converted into agricultural and settlement areas, increasing the risk of flooding.
+
+        “If nothing is done to resolve this issue or relocate people from these areas,” said Mr. Tuyishimire, “we should expect consistent lethal floods.”
+
+        Across East Africa in recent years, many areas, including in Uganda, Kenya and Somalia, have been experiencing both severe droughts and heavy rainfalls that kill many and damage properties and crops. In 2020, floods killed hundreds of people in the region.
+
+        On Wednesday, the Red Cross in Uganda said that landslides had killed six people there, as well.
+
+        Last year, a study found that human-caused climate change made heavy rains that lead to deadly floods in West Africa 80 times more likely. The scientists, part of the World Weather Attribution group, also found that the heavy rains that caused catastrophic flooding in South Africa last year had been made twice as likely by climate change.
+
+        Mouhamadou Bamba Sylla, a Rwandan climate change scientist at the African Institute for Mathematical Sciences and an author with the United Nations Intergovernmental Panel on Climate Change, said that he could not say with certainty that Tuesday’s rainfalls were associated with climate change. But in general, he noted, climate change has increased the frequency and intensity of extreme rainfalls.
+
+        The Rwandan government has promised to provide assistance to those in need, and relief efforts have already included helping bury victims and providing supplies to those whose homes were destroyed, Marie Solange Kayisire, a minister for emergencies, told reporters.
+
+        “My deepest condolences to the families and loved ones of the victims of the landslides and floods that occurred last night,” Rwanda’s president, Paul Kagame, said on Twitter. “We are doing everything within our means to address this difficult situation.”
+
+        Mr. Nsanimana’s house initially withstood the heavy rains on Tuesday, but eventually the flooding caused severe damage to its foundation, and it began to collapse.
+
+        Mr. Nsanimana decided to move his family to his brother’s house in the north of the country, but he is not sure he will be able to afford to send them to school there.
+
+        “I am now thinking of what to do next,” he said. “I don’t even know.”
+    """
     print(obj.wrapper(t))
